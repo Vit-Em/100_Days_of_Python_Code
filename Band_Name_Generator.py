43521d647e78fd4ecc65fc35a6bec2e21c71793a
Vit-Em <vitali@emelianov.de>
2025-01-29 17:49:49 +0100

    Delete Band_Name_Generator.py

--- Band_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("wellcome to the Band Name Generator!")
-city = input("Witch city did you grow up in?\n")
-pet = input("What is the name of your pet?\n")
-print("Your Band Name is"+city+" "+pet)
